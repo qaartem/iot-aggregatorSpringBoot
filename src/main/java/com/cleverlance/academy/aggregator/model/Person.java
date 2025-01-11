945da46288ec4e4ac9cda8cf8101fe9b4890916c
qaartem <artemminsadyrov@gmail.com>
2025-01-11 23:53:11 +0100

    heroku-demo

--- src/main/java/com/cleverlance/academy/aggregator/model/Person.java
+++ src/main/java/com/cleverlance/academy/aggregator/model/Person.java
@@ -1,9 +1,11 @@
 package com.cleverlance.academy.aggregator.model;
 
 
+import lombok.Data;
 import lombok.Value;
 
 @Value
+@Data
 public class Person {
     private String firstName;
     private String lastName;
